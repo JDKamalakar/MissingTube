e890220f8066e4eea54121d8a0cb4d92f49a8f05
JDKamalakar <101495154+JDKamalakar@users.noreply.github.com>
2025-07-08 11:47:19 +0530

    Updated VideoTable.tsx

--- src/components/VideoTable.tsx
+++ src/components/VideoTable.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { ArrowUpDown, ArrowUp, ArrowDown, ExternalLink, AlertTriangle, Search, FileText, Clock, Play } from 'lucide-react';
 import { Video, FilterMode } from '../types';
 import { getVideoUrl } from '../utils/youtube';
+import UnavailableImage from "../src/assets/Unavailable.png";
 import { SearchActionsModal } from './SearchActionsModal';
 import { VideoDescriptionModal } from './VideoDescriptionModal';
-import UnavailableImage from "./src/assets/Unavailable.png";
 
 
 interface VideoTableProps {
