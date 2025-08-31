ba021654bd95a83fcb02dd166a525bc46517fe80
JDKamalakar <101495154+JDKamalakar@users.noreply.github.com>
2025-08-31 10:10:05 +0530

    Updated ThemeToggle.tsx

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -69,7 +69,7 @@ export const ThemeToggle: React.FC = () => {
         aria-label="Toggle theme"
       >
         <div className={`relative flex items-center justify-center transition-all duration-300 ease-in-out ${
-            isScrolled ? 'w-6 h-6' : 'w-10 h-10 sm:w-6 sm:h-6'
+            isScrolled ? 'w-6 h-6' : 'w-14 h-14 sm:w-6 sm:h-6'
           } ${showOptions ? 'rotate-[360deg]' : 'rotate-0'}`}
         >
           <Monitor
