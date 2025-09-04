c77686df95b0f2cde0817e2a33cfc77b161aa0cd
JDKamalakar <101495154+JDKamalakar@users.noreply.github.com>
2025-09-04 10:31:01 +0530

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -228,4 +228,4 @@ export const Navbar: React.FC<NavbarProps> = ({
       {showComparisonModal && (<ComparisonModal onClose={() => { setShowComparisonModal(false); setActiveNavItem(null); }} currentVideos={currentVideos} currentPlaylistInfo={currentPlaylistInfo} />)}
     </>
   );
-};
+};111
